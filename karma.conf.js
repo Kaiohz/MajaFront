21ecbc5692cddf0a034a1bae8f8e8262847c2940
Kaiohz <goncalves.yohan@gmail.com>
2021-11-18 21:33:01 +0100

    change karma conf headless chrome

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['MyHeadlessChrome'],
     singleRun: false
   });
 };
