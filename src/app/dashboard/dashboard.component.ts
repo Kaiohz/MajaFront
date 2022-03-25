6e78e81aa141c67076c024a3fe4adbf98cb08679
Yohan GONCALVES <M89156@bpifrance.fr>
2022-03-25 09:39:50 +0100

    change elec price

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -92,7 +92,7 @@ export class DashboardComponent implements OnInit {
               }else if(device.powerUsage > 0 && device.speeds[0] != undefined){
                   this.hTotal.next(this.hTotal.getValue()+Number.parseInt(device.speeds[0].speed.substring(0,8)))
                   this.pTotal.next(this.pTotal.getValue()+device.powerUsage)
-                  this.eTotal.next(Number((this.eTotal.getValue()+(((24*device.powerUsage)/1000)*0.17)).toFixed(2)))  
+                  this.eTotal.next(Number((this.eTotal.getValue()+(((24*device.powerUsage)/1000)*0.19)).toFixed(2)))  
               }
             })
           })
