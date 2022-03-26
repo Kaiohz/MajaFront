d9a4fece2712e6c78083ef69f1cb0cd121af0fc9
kaiohz <kaiohz@mail.com>
2022-03-26 22:00:14 +0100

    add rigs component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -64,7 +64,7 @@ export class DashboardComponent implements OnInit {
   loadDashboard() {
     this.clearTotal()
     this.getInfosBtcAddress();
-    
+    this.getAverages();
   }
 
   getAverages() {
@@ -168,6 +168,7 @@ export class DashboardComponent implements OnInit {
         this.eurosYear.next((this.euros.getValue()*30)*12)
         this.profitabilityEuros.next((this.profitability.getValue()/this.changeRate.getValue()).toFixed(2))
         this.balance.next((this.balanceBTC.getValue()/this.changeRate.getValue()).toFixed(2))
+        this.getAverages()
       },
       error: err => {
         console.log("Erreur communication api change rate : "+err)
