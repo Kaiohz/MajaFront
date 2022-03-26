1ef2216cada8203d2d3cc62767d1911b0670a882
kaiohz <kaiohz@mail.com>
2022-03-27 00:50:42 +0100

    test charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -51,8 +51,64 @@ export class DashboardComponent implements OnInit {
 
   constructor(private niceHashService: NiceHashService) { }
 
+  startAnimationForLineChart(chart){
+    let seq: any, delays: any, durations: any;
+    seq = 0;
+    delays = 80;
+    durations = 500;
+
+    chart.on('draw', function(data) {
+      if(data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      } else if(data.type === 'point') {
+            seq++;
+            data.element.animate({
+              opacity: {
+                begin: seq * delays,
+                dur: durations,
+                from: 0,
+                to: 1,
+                easing: 'ease'
+              }
+            });
+        }
+    });
+
+    seq = 0;
+};
 
   ngOnInit() {
+          /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+
+          const dataDailySalesChart: any = {
+            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+            series: [
+                [12, 17, 7, 17, 23, 18, 38]
+            ]
+        };
+  
+       const optionsDailySalesChart: any = {
+            lineSmooth: Chartist.Interpolation.cardinal({
+                tension: 0
+            }),
+            low: 0,
+            high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+        }
+  
+        var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+  
+        this.startAnimationForLineChart(dailySalesChart);
+
+        
     this.breakpoint = (window.innerWidth <= this.breakpointWidth) ? 3 : 5;
     this.breakpointRig =  (window.innerWidth <= this.breakpointWidth) ? 7 : 8;
     this.dashboardClass = this.breakpoint > 3 ? 'dashboard' : 'dashboard-smartphone' ;
