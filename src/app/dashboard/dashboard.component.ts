09f96b52ef9b070a1a933ca442ba643140054f72
kaiohz <kaiohz@mail.com>
2022-03-27 04:00:45 +0200

    test charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -108,26 +108,26 @@ export class DashboardComponent implements OnInit {
 
     chart.on('draw', function(data) {
       if(data.type === 'line' || data.type === 'area') {
-        data.element.animate({
-          d: {
-            begin: 600,
-            dur: 700,
-            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-            to: data.path.clone().stringify(),
-            easing: Chartist.Svg.Easing.easeOutQuint
-          }
-        });
+        // data.element.animate({
+        //   d: {
+        //     begin: 600,
+        //     dur: 700,
+        //     from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+        //     to: data.path.clone().stringify(),
+        //     easing: Chartist.Svg.Easing.easeOutQuint
+        //   }
+        // });
       } else if(data.type === 'point') {
             seq++;
-            data.element.animate({
-              opacity: {
-                begin: seq * delays,
-                dur: durations,
-                from: 0,
-                to: 1,
-                easing: 'ease'
-              }
-            });
+            // data.element.animate({
+            //   opacity: {
+            //     begin: seq * delays,
+            //     dur: durations,
+            //     from: 0,
+            //     to: 1,
+            //     easing: 'ease'
+            //   }
+            // });
         }
     });
 
