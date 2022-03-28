ff4bc5c7f3e1ce348f9b89c7d5fa495373f08a34
Yohan GONCALVES <M89156@bpifrance.fr>
2022-03-28 22:46:10 +0200

    set max charts automatically

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -67,7 +67,7 @@ export class DashboardComponent implements OnInit {
             tension: 0
         }),
         low: 0,
-        high: 150, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        high: Math.max(serie)+20, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
         chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
     }
 
@@ -91,7 +91,7 @@ export class DashboardComponent implements OnInit {
           tension: 0
       }),
       low: 0,
-      high: 1300, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      high: Math.max(serie)+20, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
       chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
   }
 
