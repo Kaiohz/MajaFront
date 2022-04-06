fec4b7230285b985c81ac36bc8023bbde85be0ec
Yohan GONCALVES <M89156@bpifrance.fr>
2022-04-06 21:01:49 +0200

    change refresh time

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -142,7 +142,7 @@ export class DashboardComponent implements OnInit {
     this.breakpointRig =  (window.innerWidth <= this.breakpointWidth) ? 7 : 8;
     this.dashboardClass = this.breakpoint > 3 ? 'dashboard' : 'dashboard-smartphone' ;
     this.loadDashboard();
-    setInterval(() => this.loadDashboard(), 300000);
+    setInterval(() => this.loadDashboard(), 180000);
   }
   
 
