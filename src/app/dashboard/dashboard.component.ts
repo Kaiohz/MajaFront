f88f063a36eab7f3da9cf0975c3dc1b2bcbe467d
kaiohz <kaiohz@mail.com>
2022-05-03 23:09:39 +0200

    try nextPayout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -219,7 +219,7 @@ export class DashboardComponent implements OnInit {
         var next = new Date(this.results.nextPayoutTimestamp).getTime()
         var now = new Date().getTime()
         var remaining = new Date(next - now)
-        this.nextPayout.next(remaining.getHours()+"h"+remaining.getMinutes()+"m")
+        this.nextPayout.next(remaining.getMinutes()%60+"h"+remaining.getMinutes()+"m")
         this.devicesMining.next(this.results.devicesStatuses.MINING)
         this.rigMining.next(this.results.minerStatuses.MINING)
         this.rigOffline.next(this.results.minerStatuses.OFFLINE)
