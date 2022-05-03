dbc0ae9acba66f1dc0344e74e57755bed81818a0
kaiohz <kaiohz@mail.com>
2022-05-03 23:34:08 +0200

    try nextPayout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -217,10 +217,9 @@ export class DashboardComponent implements OnInit {
       next: value => {
         this.results = <NiceHashObject>value;
         var next = new Date(this.results.nextPayoutTimestamp).getTime()
-        console.log("Next : ", new Date(this.results.nextPayoutTimestamp).getTimezoneOffset())
         var now = new Date().getTime()
-        var remaining = new Date(next - now).getMinutes()
-        var hoursRemaining = (remaining/60).toFixed(0)
+        var remaining = new Date(next - now)
+        var hoursRemaining = ((remaining.getMinutes()-remaining.getTimezoneOffset())/60).toFixed(0)
         this.nextPayout.next(hoursRemaining+"h"+remaining+"m")
         this.devicesMining.next(this.results.devicesStatuses.MINING)
         this.rigMining.next(this.results.minerStatuses.MINING)
